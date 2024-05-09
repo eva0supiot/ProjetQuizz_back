7b014f11972b92bdb8ccf3c06e1801fce8df0ccd
vsuspicious <norianeberne@gmail.com>
2024-05-09 15:30:42 +0200

    ajouts quizzes

--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -2,21 +2,99 @@ INSERT INTO utilisateurs (pseudo, mdp, admin, scores, pdp) VALUES ('eva', 'sup00
 INSERT INTO utilisateurs (pseudo, mdp, admin, scores, pdp) VALUES ('dorian', 'cro00', FALSE, '1:3/3-2:0/1', 'PhotoProfil4');
 INSERT INTO utilisateurs (pseudo, mdp, admin, scores, pdp) VALUES ('noriane', 'ber00', TRUE, '1:3/3-2:0/1', 'PhotoProfil5');
 
+
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('France', TRUE, 1);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Pays-Bas', FALSE, 1);
+
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Emmanuel Macron', FALSE, 2);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Anne Hidalgo', TRUE, 2);
+
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Tour Eiffel', FALSE, 3);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Arc de Triomphe', False, 3);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Palais de lElysée', FALSE, 3);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('Sagradafamilia', TRUE, 3);
+
+
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Tamise', True, 4);
 INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Seine', False, 4);
+
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Titanic', True, 5);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Inception', False, 5);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Lupin', False, 6);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Intouchables', True, 6);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Le Jeu de la Dame', False, 7);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Imitation Game', True, 7);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Ca', True, 8);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Conjuring', False, 8);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Coup de Foudre à Notting Hill', False, 9);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Pretty Woman', True, 9);
+
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Border Collie', True, 10);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Lévrier', False, 10);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Golden Retriever', False, 11);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Berger Australien', True, 11);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Lévrier', True, 12);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Malinois', False, 12);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Corgi', False, 13);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Golden Retriever', True, 13);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Berger Allemand', False, 14);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Dogue Allemand', True, 14);
+
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Belle et la Bête', True, 15);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Belle et le Clochard', False, 15);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Princesse et la Grenouille', False, 16);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Vaiana', True, 16);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Rebelle', True, 17);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Cendrillon', False, 17);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Belle et le Clochard', True, 18);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('La Planète au Trésor', False, 18);
+
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Avengers', False, 19);
+INSERT INTO reponses (contenu, solution, question_id) VALUES ('Les Nouveaux Héros', True, 19);
+
+
 
 INSERT INTO questions (contenu, image, quizz_id) VALUES ('De quel pays Paris est-elle la capitale ?', NULL, 1);
 INSERT INTO questions (contenu, image, quizz_id) VALUES ('Qui est le maire de Paris actuellement ?', NULL, 1);
 INSERT INTO questions (contenu, image, quizz_id) VALUES ('Lequel de ces monuments ne se trouve pas à Paris ?', NULL, 1);
+
 INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel est le nom du fleuve qui traverse Londres ?', NULL, 2);
+
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film avec Leonardo DiCaprio se déroule sur un bâteau entrant en collision avec un iceberg ?', NULL, 3);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film français avec Omar Sy traite d une rencontre improbable entre un homme tétraplégique et un jeune de banlieue ?', NULL, 3);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film traite du décryptage de la machine Enigma, emblématique de la Seconde Guerre Modiale ?', NULL, 3);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film d horreur a pour antagoniste principal un clown ?', NULL, 3);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film traite de la rencontre entre une prostituée et un millionnaire ?', NULL, 3);
+
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quelle race de chien est le plus souvent employée en chiens de berger ?', NULL, 4);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quelle fut la race de chien la plus adoptée en France en 2021 ?', NULL, 4);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Laquelle de ces race de chien est la plus adaptée à la course ?', NULL, 4);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quelle est la race de chien considérée comme la plus adaptée à la vie de famille ?', NULL, 4);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quels sont les chiens les plus grands du monde ?', NULL, 4);
+
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel fut le premier Disney avec un personnage ouvertement homosexuel ?', NULL, 5);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film Disney s inspire librement de la mythologie polynésienne ?', NULL, 5);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Dans quel film Disney une princesse doit-elle rompre un terrible sort s étant abattu sur sa mère ?', NULL, 5);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('De quel film provient la scène culte de la dégustation de spagetti ?', NULL, 5);
+INSERT INTO questions (contenu, image, quizz_id) VALUES ('Quel film Disney provient la phrase "Je suis satisfait de mes soins." ?', NULL, 5);
+
 
 INSERT INTO quizzes (titre) VALUES ('Tout sur Paris !');
 INSERT INTO quizzes (titre) VALUES ('Tout sur Londres !');
+INSERT INTO quizzes (titre) VALUES ('Culture film !');
+INSERT INTO quizzes (titre) VALUES ('Les races de chiens !');
+INSERT INTO quizzes (titre) VALUES ('Les films Disney !');
